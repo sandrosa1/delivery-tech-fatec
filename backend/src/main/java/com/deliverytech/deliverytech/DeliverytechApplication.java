ad570eae7d3a2f52072ea9e8c748ad78c67c1b75
sandrosa1 <sandrosa@fatec.sp.gov.br>
2021-04-02 17:29:04 -0300

    Projeto iniciado

---
+++ backend/src/main/java/com/deliverytech/deliverytech/DeliverytechApplication.java
@@ -0,0 +1,13 @@
+package com.deliverytech.deliverytech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverytechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverytechApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/deliverytech/deliverytech/DeliverytechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliverytech.deliverytech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverytechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
