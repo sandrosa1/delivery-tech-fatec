05dbc65286bc585dbc350abe409c044ba1bdab7a
sandrosa1 <sandrosa@fatec.sp.gov.br>
2021-04-02 22:10:46 -0300

    endpoint post/orders

--- backend/src/main/java/com/deliverytech/deliverytech/services/OrderService.java
+++ backend/src/main/java/com/deliverytech/deliverytech/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.deliverytech.deliverytech.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.deliverytech.deliverytech.dto.OrderDTO;
+import com.deliverytech.deliverytech.dto.ProductDTO;
 import com.deliverytech.deliverytech.entities.Order;
+import com.deliverytech.deliverytech.entities.OrderStatus;
+import com.deliverytech.deliverytech.entities.Product;
 import com.deliverytech.deliverytech.repositories.OrderRepository;
+import com.deliverytech.deliverytech.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,9 +22,25 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository ProductRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findOrdersWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order (null, dto.getAddress(),dto.getLatitude(), dto.getLongitude(),
+				Instant.now(), OrderStatus.PENDING);
+		for(ProductDTO p : dto.getProducts()) {
+			Product product = ProductRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = repository.save(order);
+		return new OrderDTO(order);
+	}
+	
 }
