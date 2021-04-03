c0b85f6a080f4426771aacdfe4dacf790558b1f3
sandrosa1 <sandrosa@fatec.sp.gov.br>
2021-04-02 21:38:16 -0300

    endpoint Order

---
+++ backend/src/main/java/com/deliverytech/deliverytech/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.deliverytech.deliverytech.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.deliverytech.deliverytech.dto.OrderDTO;
+import com.deliverytech.deliverytech.entities.Order;
+import com.deliverytech.deliverytech.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+}
