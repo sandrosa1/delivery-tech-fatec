d8b0edc8b06966afc9635fc174bf0778402b15c6
Sandro Amâncio de Sá <56263980+sandrosa1@users.noreply.github.com>
2021-06-22 18:51:03 -0300

    Update trabalho.sql

--- database/trabalho.sql
+++ database/trabalho.sql
@@ -26,6 +26,64 @@ Go
 
 
 
+
+
+
+
+
+
+
+
+
+---Criando uma regra de preço--
+Select * From tb_Produto 
+Where NomeProduto = 'Mussarela'
+Go
+
+Update tb_Produto
+Set ValorUnitarioProduto = - 55
+Where NomeProduto = 'Mussarela'
+Go
+
+Select * From tb_Produto 
+Where NomeProduto = 'Mussarela'
+Go
+
+Update tb_Produto
+Set ValorUnitarioProduto =  55
+Where NomeProduto = 'Mussarela'
+Go
+
+
+Create Rule RegraParaPreco As @Valor > 0
+Go
+
+Execute sp_bindrule RegraParaPreco, 'tb_Produto.ValorUnitarioProduto'
+Go
+
+Update tb_Produto
+Set ValorUnitarioProduto = - 55
+Where NomeProduto = 'Mussarela'
+Go
+
+
+Update tb_Produto
+Set ValorUnitarioProduto =  60
+Where NomeProduto = 'Mussarela'
+Go
+
+
+
+
+
+
+
+
+
+
+
+
+
 -- Verificando os produtos que mais foram pedidos
 Select B.NomeProduto, B.TipoProduto, Count(A.CodigoProdutoPedido) as Ocorrencia
 			from dbo.tb_Pedido A
@@ -34,6 +92,8 @@ Select B.NomeProduto, B.TipoProduto, Count(A.CodigoProdutoPedido) as Ocorrencia
 					Order By [Ocorrencia] Desc
 
 Go
+
+
 
 
 
@@ -99,6 +159,71 @@ Go
 
 
 
+
+--Criando uma pesquisa 
+Create Procedure 
+Pesq_Produto (@p Varchar(10))
+As
+
+Select * From tb_Produto
+Where NomeProduto =  @p
+Go
+
+Exec Pesq_Produto 'MUsSarela'
+Go
+
+
+
+
+
+
+
+
+
+
+
+
+
+--Pesquisa muito utilizada
+Alter Procedure 
+Pesq_Produto (@p Varchar(1))
+As
+
+
+Select * From tb_Produto
+Where NomeProduto Like @p+'%'
+Go
+
+
+
+Exec Pesq_Produto 'M'
+Go
+
+
+
+
+
+
+
+--Sugestão de melhoria--
+Create Index NossosProdutos On tb_Produto(NomeProduto)
+Go
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 -- Verificando o valor total arrecadado dos Produtos mais pedidos
 Select B.CodigoProduto, B.NomeProduto, Count(A.CodigoProdutoPedido) as Ocorrencia,B.ValorUnitarioProduto as 'Valor Unitário', Format(SUM(A.ValorUnitarioPedido),'C','pt-br') as 'Total Arrecadado'
 			from dbo.tb_Pedido A
@@ -149,6 +274,10 @@ Go
 
 
 
+
+
+
+
 --Buscando Todos so Clientes
 --Tems um Problema
 Select NomeCliente,  TelefoneCliente From tb_Cliente Group By  NomeCliente, TelefoneCliente
@@ -183,6 +312,10 @@ Go
 
 
 
+
+
+
+
 --Calculando o valor total de cada pedido--
 
 Select * From tb_Cliente C
@@ -202,12 +335,15 @@ Go
 
 
 
+
 --Buscando os 5 clientes que mais gastou em um pedido no delivery--
 Select Top 5 NomeCliente, TelefoneCliente, Format([Mais Gasta],'C','pt-br')  from tb_Cliente C
 Cross Apply (Select SUM(ValorTotalPedido) AS [Mais Gasta] From tb_Pedido P
                         Where C.CodigoCliente = P.CodigoClientePedido) As R
 						Order By [Mais Gasta] Desc
 Go
+
+
 
 
 
@@ -250,6 +386,36 @@ Go
 
 
 
+
+--Calculando o gasto de cada cliente no delivery Utilizando uma sub consulta
+Select  R.Cliente, SUM(R.Total) As Total
+From
+(
+	Select C.NomeCliente As Cliente, P.ValorUnitarioPedido * P.QuantidadePedido As Total
+	From tb_cliente AS C
+	Inner Join tb_Pedido As P
+	On C.CodigoCliente = P.CodigoClientePedido
+) As R
+Group By R.Cliente
+Order By Total Desc
+Go
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 --Calculando o gasto de cada cliente no delivery atraves de uma CTE
 ;
 With TotalPorCliente (NomeCliente, ValorTotalPedido)
@@ -263,6 +429,17 @@ From TotalPorCliente
 Group by NomeCliente 
 Order By ValorGasto Desc
 Go
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -421,7 +598,7 @@ Go
 
 
 --Indentificando os bairros que mais consomem --
-Create View V_MelhorRegiaoDeConsumo
+Create View  V_EnderecoCliente
 As
 Select Bairro From tb_Endereco
 Go
@@ -434,10 +611,41 @@ Go
 
 
 
+-- Criando a Visão para calcular Taxa de Entrega --
+Create View V_Taxa_De_Entrega
+As
+Select CodigoClientePedido As Pedido, ValorTotalPedido As Total, 
+            Case 
+			 When ValorTotalPedido Between 0 And 99 Then (ValorTotalPedido* 0.25)  --Taxa maior para pedidos menores--
+			 When ValorTotalPedido >= 100 Then (ValorTotalPedido * 0.15) 
+			 When ValorTotalPedido >= 200 Then (ValorTotalPedido * 0.05) --Taxa menor para pedidos maiores--
+			End As Taxa 
+From tb_Pedido
+Go
+
+-- Executar a Visão V_Taxa_De_Entrega--
+Select *  From V_Taxa_De_Entrega
+Go
+
+-- Calculando o Taxa + Pedido --
+Select Pedido, Pedido, Format(Taxa ,'C','pt-br'), Format((Total + Taxa),'C','pt-br') As 'Valor total com entrega'
+From V_Taxa_De_Entrega
+Go
 
 
 
-Select Bairro, Count(1) ocorrencias From V_MelhorRegiaoDeConsumo Group By Bairro
+
+
+
+
+
+
+
+
+
+
+--Regiao que mais faz pedido
+Select Bairro, Count(1) ocorrencias From V_EnderecoCliente Group By Bairro
 Having Count(1)  > 1
 Order By 2 Desc
 Go
@@ -449,13 +657,21 @@ Go
 
 
 
-----------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
 --Criando uma visão com os dados pertinentes para a entrega--
-Create View V_EnderecoCliente
+Alter View V_EnderecoCliente
 As
 Select NomeCliente, TelefoneCliente, TipoLogradouro, Logradouro, Numero, Bairro
 From tb_Cliente C Inner Join tb_Endereco E On C.CodigoCliente = E.CodigoEndereco
 Go
+
+
+
+
 
 
 
@@ -505,56 +721,83 @@ Go
 
 
 
+
+
+
 -- Utilizando Funções defindas pelo Usuário (User Defined Functions)
--- Função Multi-Statement  para retornar todos os Pedidos feitos por cliente .
+-- Função Multi-Statement para retornar todos os itens de Pedidos feitos por cliente .
 CREATE FUNCTION F_RetornaPedidosPorCliente(@CodigoCliente Int)
 RETURNS @Resultado Table
-		(CodigoProdutoPedido  Int, 
+		(CodigoProdutoPedido Int,
+		Categoria Varchar(50),
+		NomeProduto Varchar (50),
+		Quantidade INT,
 		NomeCliente Varchar(50),
 		MomentoPedido DateTime)
 AS
 BEGIN
  INSERT @Resultado
- SELECT PE.CodigoProdutoPedido, CL.NomeCliente, CL.MomentoPedido
+ SELECT PE.CodigoProdutoPedido, PR.CategoriaProduto, PR.NomeProduto, PE.QuantidadePedido,CL.NomeCliente, CL.MomentoPedido
  FROM tb_Pedido PE INNER JOIN tb_Cliente CL
 				 ON PE.CodigoClientePedido = CL.CodigoCliente
+				 INNER JOIN tb_Produto PR
+				 ON PR.CodigoProduto = PE.CodigoProdutoPedido
  WHERE PE.CodigoClientePedido = @CodigoCliente
  RETURN
 END
 GO
 
 -- Listando todos os produtos pedidos pelo cliente e a data
-select * from F_RetornaPedidosPorCliente(3)
-Go
+SELECT * FROM F_RetornaPedidosPorCliente(111)
+GO
 
 -- Mostrando a quantidade total de pedidos realizados até o momento pelo cliente
-select count(*) as 'Quantidade de pedidos realizados' from F_RetornaPedidosPorCliente(3) 
-Go
+SELECT SUM(Quantidade) AS 'Quantidade de itens pedidos' FROM F_RetornaPedidosPorCliente(111) 
+GO
 
--- Função Multi-Statement para retornar todos os Pedidos por Bairro.
+-- Mostrando a quantidade total de pedidos realizados após determinada data
+SELECT SUM(Quantidade) AS 'Quantidade de itens pedidos' FROM F_RetornaPedidosPorCliente(111) 
+where MomentoPedido >= CONVERT(DATETIME, '2021-04-01' )
+GO
+
+
+
+
+
+
+
+
+
+-- Função Multi-Statement para retornar todos itens de Pedidos por Bairro.
 CREATE FUNCTION F_RetornaPedidosPorBairro(@Bairro Varchar(30))
 RETURNS @Regioes Table
 		(CodigoEndereco Int,
 		 Bairro Varchar(30),
 		 TipoLogradouro Varchar(5),
 		 Logradouro Varchar(100),
-		 MomentoPedido DateTime)
+		 MomentoPedido DateTime,
+		 CategoriaProduto Varchar(50),
+		 NomeProduto Varchar(50),
+		 Quantidade INT)
 AS
 BEGIN
  INSERT @Regioes
- SELECT EN.CodigoEndereco, EN.Bairro, EN.TipoLogradouro, EN.Logradouro, CL.MomentoPedido
+ SELECT EN.CodigoEndereco, EN.Bairro, EN.TipoLogradouro, EN.Logradouro, CL.MomentoPedido, PR.CategoriaProduto, PR.NomeProduto, PE.QuantidadePedido
  FROM tb_Endereco EN INNER JOIN tb_Cliente CL
 					 ON EN.CodigoEndereco = CL.CodigoEndereco
 					 INNER JOIN tb_Pedido PE
 					 ON CL.CodigoCliente = PE.CodigoClientePedido
+					 INNER JOIN tb_Produto PR
+					 ON PE.CodigoProdutoPedido = PR.CodigoProduto
 WHERE EN.Bairro = @Bairro
 RETURN
 END
 GO
 
-select * from F_RetornaPedidosPorBairro('Centro')
-Go
+-- Mostra os itens pedidos por bairro
+SELECT * FROM F_RetornaPedidosPorBairro('Centro')
+GO
 
-select count(*) from F_RetornaPedidosPorBairro('Centro')
-Go
-
+-- Mostra o número de itens pedidos por bairro
+SELECT SUM(Quantidade) AS 'Quantidade de itens pedidos' FROM F_RetornaPedidosPorBairro('Centro')
+GO
